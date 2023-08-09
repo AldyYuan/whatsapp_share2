7fe9ba3bfb8df7135d828926316ef7df0f050bdb
AldyYuan <yuanaldy@gmail.com>
2023-08-09 10:51:25 +0700

    prepare the intent first

--- android/src/main/java/com/example/whatsapp_share/WhatsappShare.java
+++ android/src/main/java/com/example/whatsapp_share/WhatsappShare.java
@@ -192,6 +192,41 @@ public class WhatsappShare implements FlutterPlugin, MethodCallHandler {
                 files.add(fileUri);
             }
 
+            // Flag to control further code execution
+            boolean shouldContinue = true;
+
+            Intent intentReg = new Intent();
+            intentReg.setFlags(intentReg.FLAG_ACTIVITY_CLEAR_TOP);
+            intentReg.setFlags(intentReg.FLAG_ACTIVITY_NEW_TASK);
+            intentReg.setAction(intentReg.ACTION_SEND_MULTIPLE);
+            intentReg.setType("*/*");
+            intentReg.setPackage(packageName);
+            intentReg.putExtra("jid", phone + "@s.whatsapp.net");
+            intentReg.putExtra(intentReg.EXTRA_SUBJECT, title);
+            intentReg.putExtra(intentReg.EXTRA_TEXT, text);
+            intentReg.putExtra(intentReg.EXTRA_STREAM, files);
+            intentReg.addFlags(intentReg.FLAG_GRANT_READ_URI_PERMISSION);
+
+            // Intent chooserIntent = intentReg.createChooser(intent, chooserTitle);
+            intentReg.setFlags(intentReg.FLAG_ACTIVITY_CLEAR_TOP);
+            intentReg.setFlags(intentReg.FLAG_ACTIVITY_NEW_TASK);
+
+            Intent intentW4b = new Intent();
+            intentW4b.setFlags(intentW4b.FLAG_ACTIVITY_CLEAR_TOP);
+            intentW4b.setFlags(intentW4b.FLAG_ACTIVITY_NEW_TASK);
+            intentW4b.setAction(intentW4b.ACTION_SEND_MULTIPLE);
+            intentW4b.setType("*/*");
+            intentW4b.setPackage("com.whatsapp.w4b");
+            intentW4b.putExtra("jid", phone + "@s.whatsapp.net");
+            intentW4b.putExtra(intentW4b.EXTRA_SUBJECT, title);
+            intentW4b.putExtra(intentW4b.EXTRA_TEXT, text);
+            intentW4b.putExtra(intentW4b.EXTRA_STREAM, files);
+            intentW4b.addFlags(intentW4b.FLAG_GRANT_READ_URI_PERMISSION);
+
+            // Intent chooserIntent = intentW4b.createChooser(intent, chooserTitle);
+            intentW4b.setFlags(intentW4b.FLAG_ACTIVITY_CLEAR_TOP);
+            intentW4b.setFlags(intentW4b.FLAG_ACTIVITY_NEW_TASK);
+
             if (!ContactHelper.isPhoneNumberValid(phone)) {
                 // Log
                 System.out.println("Invalid phone number format");
@@ -206,38 +241,7 @@ public class WhatsappShare implements FlutterPlugin, MethodCallHandler {
                 handler.postDelayed(new Runnable() {
                     @Override
                     public void run() {
-
-                        Intent intentReg = new Intent();
-                        intentReg.setFlags(intentReg.FLAG_ACTIVITY_CLEAR_TOP);
-                        intentReg.setFlags(intentReg.FLAG_ACTIVITY_NEW_TASK);
-                        intentReg.setAction(intentReg.ACTION_SEND_MULTIPLE);
-                        intentReg.setType("*/*");
-                        intentReg.setPackage(packageName);
-                        intentReg.putExtra("jid", phone + "@s.whatsapp.net");
-                        intentReg.putExtra(intentReg.EXTRA_SUBJECT, title);
-                        intentReg.putExtra(intentReg.EXTRA_TEXT, text);
-                        intentReg.putExtra(intentReg.EXTRA_STREAM, files);
-                        intentReg.addFlags(intentReg.FLAG_GRANT_READ_URI_PERMISSION);
-
-                        // Intent chooserIntent = intentReg.createChooser(intent, chooserTitle);
-                        intentReg.setFlags(intentReg.FLAG_ACTIVITY_CLEAR_TOP);
-                        intentReg.setFlags(intentReg.FLAG_ACTIVITY_NEW_TASK);
-
-                        Intent intentW4b = new Intent();
-                        intentW4b.setFlags(intentW4b.FLAG_ACTIVITY_CLEAR_TOP);
-                        intentW4b.setFlags(intentW4b.FLAG_ACTIVITY_NEW_TASK);
-                        intentW4b.setAction(intentW4b.ACTION_SEND_MULTIPLE);
-                        intentW4b.setType("*/*");
-                        intentW4b.setPackage("com.whatsapp.w4b");
-                        intentW4b.putExtra("jid", phone + "@s.whatsapp.net");
-                        intentW4b.putExtra(intentW4b.EXTRA_SUBJECT, title);
-                        intentW4b.putExtra(intentW4b.EXTRA_TEXT, text);
-                        intentW4b.putExtra(intentW4b.EXTRA_STREAM, files);
-                        intentW4b.addFlags(intentW4b.FLAG_GRANT_READ_URI_PERMISSION);
-
-                        // Intent chooserIntent = intentW4b.createChooser(intent, chooserTitle);
-                        intentW4b.setFlags(intentW4b.FLAG_ACTIVITY_CLEAR_TOP);
-                        intentW4b.setFlags(intentW4b.FLAG_ACTIVITY_NEW_TASK);
+                        System.out.println("Contact saved and delay completed");
 
                         try {
                             context.startActivity(intentW4b);
@@ -245,9 +249,21 @@ public class WhatsappShare implements FlutterPlugin, MethodCallHandler {
                             context.startActivity(intentReg);
                         }
 
+                        shouldContinue = false;
+
                         result.success(true);
                     }
                 }, 1000);
+            }
+
+            if (shouldContinue) {
+                try {
+                    context.startActivity(intentW4b);
+                } catch (Exception ex) {
+                    context.startActivity(intentReg);
+                }
+
+                result.success(true);
             }
 
         } catch (Exception ex) {
